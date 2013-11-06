29b3b4b2753d0ab4f23d22c4e64e0b6dcc4f85bf
Thomas Giles <wthit56@googlemail.com>
2013-11-06 16:04:59 +0000

    removed PixelFarm

--- PixelFarm/PixelFarm.js
+++
@@ -1,7 +0,0 @@
-(function () {
-	var canvas = document.createElement("CANVAS");
-	if (!canvas.getContext || !canvas.getContext("2d")) { throw "Browser does not have 2D canvas support."; }
-	canvas = null;
-})();
-
-
