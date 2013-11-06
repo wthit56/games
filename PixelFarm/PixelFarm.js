2d1a9fe78775adf7ea3d88015acb737319f9fd8c
Thomas Giles <wthit56@googlemail.com>
2013-11-06 16:02:59 +0000

    PixelFarm exploration

---
+++ PixelFarm/PixelFarm.js
@@ -0,0 +1,7 @@
+(function () {
+	var canvas = document.createElement("CANVAS");
+	if (!canvas.getContext || !canvas.getContext("2d")) { throw "Browser does not have 2D canvas support."; }
+	canvas = null;
+})();
+
+
